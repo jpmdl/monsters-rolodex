563e7dc17e59dc1a30e3291ebba85bff771a2c56
João Loureiro <jpmdl@hotmail.com>
2020-01-19 08:39:00 +0000

    Updates

---
+++ precache-manifest.997495dda88857ce1ff1b1e88d785c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af69017091f61937ebbd8befb5ec8cc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fd9507d7eebdd525410b",
+    "url": "/monsters-rolodex/static/css/main.994733d0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd9507d7eebdd525410b",
+    "url": "/monsters-rolodex/static/js/main.181850d4.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
